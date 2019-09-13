9f769d7bc9e8bbffbf1bfb28ee8587b0a01227f4
Melanie Nolan <melanie.nolan@casumo.com>
2019-09-13 18:05:09 +0200

    config: change theme color and path to layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,11 +29,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `gatsby-starter-blog`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#27495A`,
+        theme_color: `#27495A`,
         display: `minimal-ui`,
         // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
@@ -48,7 +48,7 @@ module.exports = {
       resolve: `gatsby-plugin-mdx`,
       options: {
         defaultLayouts: {
-          default: require.resolve(`${__dirname}/src/components/Layout.js`),
+          default: require.resolve(`./src/components/Layout.js`),
         },
       },
     },
