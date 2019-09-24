60190ff1575dbccb47e23fc51e68b7c9b807c905
Melanie Nolan <melanie.nolan@casumo.com>
2019-09-24 18:21:00 +0200

    config: remove duplicate plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,8 +6,6 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     `gatsby-plugin-emotion`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
