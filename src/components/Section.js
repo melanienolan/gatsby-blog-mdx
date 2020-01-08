c4234b7cd9bd8e3fb90a19dd7a72a08b81aabadf
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-01-08 17:12:31 +0000

    Section: make section height and background consistent in all pages

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,16 +1,16 @@
 import React from "react"
 import { css } from "@emotion/core"
 
-const Section = ({ children, page }) => (
+const Section = ({ children }) => (
   <section
     css={css`
       width: 900px;
       max-width: 90vw;
       margin: 0 auto;
-      height: ${page === "index" ? "calc(80vh - 70px)" : "calc(90vh - 70px)"};
+      height: calc(80vh - 70px);
       padding: 25px;
       overflow: scroll;
-      background-color: ${page === "index" ? "transparent" : "#ffffff"};
+      background-color: transparent;
     `}
   >
     {children}
