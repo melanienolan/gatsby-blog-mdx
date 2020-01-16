9cd508d8ba03e4defd17f0c2f26df18cff043033
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-01-16 22:14:11 +0000

    Footer: fix footer styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,12 @@ import { css } from "@emotion/core"
 const Footer = () => (
   <footer
     css={css`
+      display: flex;
+      justify-content: flex-end;
+      align-items: center;
       width: 900px;
       max-width: 90vw;
+      height: 50px;
       margin: 0 auto;
       color: #ffffff;
     `}
